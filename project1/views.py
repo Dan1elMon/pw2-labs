f2667f5539a155a60b54070175fed86d180c4e2a
Daniel Chura <dchuramo@unsa.edu.pe>
2023-06-19 21:01:14 -0500

    probando pagina

--- project1/views.py
+++ project1/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 
 def saludo(request):
-    return HttpResponse("Hola mundo.");
+    ##return HttpResponse("Hola mundo.");
+    documento="""""<html>
+    <body>
+    <h1>Hola mundo
+    </h1>
+    </body>
+    </html>""""
+    def despedida(request):
+    return HttpResponse("Hasta luego");
